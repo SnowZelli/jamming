d6c434e68967fff6ce986565e837083c79407124
Nelson Bozzelli <nrbozzelli@gmail.com>
2019-09-09 19:49:06 -0600

    Created a placeholder method for saving the playlist to a user's account.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -37,6 +38,10 @@ class App extends React.Component {
     this.setState({ playlistName: name })
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -49,7 +54,8 @@ class App extends React.Component {
             <Playlist playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
-              onNameChange={this.updatePlaylistName} />
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
